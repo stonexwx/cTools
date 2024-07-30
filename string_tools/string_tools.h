aadb0820026d5b3b2f49e68c1826a7dd046e8a56
xwx <1162027477@qq.com>
2024-07-30 14:34:15 +0800

    【调整】注释

--- string_tools/string_tools.h
+++ string_tools/string_tools.h
@@ -13,7 +13,7 @@ using namespace std;
 namespace stringTools
 {
     /**
-     *  @brief 删除字符串左边指定的字符。
+     *  @brief 删除字符串左边指定的字符。（c风格）
      *   例如：str = "  hello world!  ";
      *   删除左边的空格：str = "hello world!  ";
      *  @param str 待处理的字符串。
@@ -22,6 +22,14 @@ namespace stringTools
      */
 
     char *deletelchr(char *str, const int cc = ' ');
+    /**
+     *  @brief 删除字符串左边指定的字符。（c++风格）
+     *   例如：str = "  hello world!  ";
+     *   删除左边的空格：str = "hello world!  ";
+     *  @param str 待处理的字符串。
+     *  @param cc 待删除的字符。
+     *  @return 返回处理后的字符串。
+     */
     string &deletelchr(string &str, const int cc = ' ');
 } // namespace stringTools
 
